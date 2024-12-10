f1b6130cf78423676e02651433d11c31e5fde4ce
Rodrigo Benitez <roddrigo.benitez@gmail.com>
2024-12-10 18:42:43 -0300

    capa de Users completado

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 
 import mainRoutes from "./main";
 import authRoutes from "./auth";
-import userRoutes from "../models/user/controller";
+import userRouter from "../models/user/routes";
 
 const router = express.Router();
 
 router.use("/", mainRoutes);
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
+router.use("/user", userRouter);
 
 export default router;
