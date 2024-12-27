e1b6799c45c5a01237e596e912af06ee22539755
Rodrigo Benitez <roddrigo.benitez@gmail.com>
2024-12-27 00:02:49 -0300

    Order model/Types

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,5 +1,9 @@
 export type UserRole = "caja" | "mesero" | "cocina"
 
+export type AreaRole = "caja" | "barra" | "cocina"
+
+export type OrderStatus = "pending" | "preparing" | "ready" | "completed"
+
 export interface IUser {
     username: string;
     password: string;
@@ -15,4 +19,16 @@ export interface IProduct{
     stock?: number;
     image?: string[];
     categoryId?: string;
+}
+
+export interface IOrder{
+    tableNumber: number,
+    products: {
+        productId: string,
+        quantity: number
+    }[],
+    status: OrderStatus,
+    userId?: string,
+    area: AreaRole,
+    createdAt: Date,
 }
