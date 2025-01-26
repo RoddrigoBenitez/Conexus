55dadd3e51696cc40a9c6b3d8343e23c1a5f0a04
Rodrigo Benitez <roddrigo.benitez@gmail.com>
2025-01-25 22:28:34 -0300

    front get the Clients

--- frontend/my-app/auth.ts
+++ frontend/my-app/auth.ts
@@ -16,7 +16,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           password: string;
         };
         try {
-          const response = await fetch(`${process.env.API_URL}/user/login`, {
+          const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password }),
---
+++ frontend/my-app/src/types/index.ts
@@ -0,0 +1,4 @@
+export interface ITable{
+    _id: string
+    tableNumber: number
+}
